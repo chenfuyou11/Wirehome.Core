5939f15acc5c6a30c5217c448602fe8e1c658f2f
bigdnf <dominik.jeske@hotmail.com>
2018-05-25 23:46:38 +0200

    debug bridge

---
+++ Adapters/AdaptersContainer/Adapters/DibugBridge/DebugBridgeAdapter.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Wirehome.ComponentModel.Capabilities;
+using Wirehome.ComponentModel.Commands;
+using Wirehome.ComponentModel.Commands.Responses;
+using Wirehome.Core.EventAggregator;
+using Wirehome.Core.Services;
+using Wirehome.Extensions.Messaging;
+using Wirehome.Model.ComponentModel.Capabilities.Constants;
+using Wirehome.Model.Extensions;
+
+namespace Wirehome.ComponentModel.Adapters.Denon
+{
+    public class DebugBridgeAdapter : Adapter
+    {
+        private readonly ISerialMessagingService _serialMessagingService;
+        
+
+        public DebugBridgeAdapter(IAdapterServiceFactory adapterServiceFactory) : base(adapterServiceFactory)
+        {
+            _serialMessagingService = adapterServiceFactory.GetUartService();
+        }
+
+        public override async Task Initialize()
+        {
+            await base.Initialize().ConfigureAwait(false);
+
+            var _i2cAddress = Properties[AdapterProperties.I2cAddress].Value.ToIntValue();
+
+            _serialMessagingService.RegisterBinaryMessage(DebugMessage.Empty);
+            _disposables.Add(_eventAggregator.SubscribeAsync<DebugMessage>(DebugChangeHandler, RoutingFilter.MessageRead));
+        }
+
+        public Task DebugChangeHandler(IMessageEnvelope<DebugMessage> message) => _eventAggregator.Publish(message.Message);
+        
+        protected DiscoveryResponse DiscoverCapabilitiesHandler(Command message)
+        {
+            return new DiscoveryResponse(RequierdProperties(), new CurrentState(ReadWriteModeValues.Read));
+        }
+    }
+}
---
+++ Adapters/AdaptersContainer/Adapters/DibugBridge/DebugMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Wirehome.ComponentModel.ValueTypes;
+using Wirehome.Core.Interface.Messaging;
+using Wirehome.Core.Interface.Native;
+
+namespace Wirehome.Extensions.Messaging
+{
+    public class DebugMessage : IBinaryMessage
+    {
+        public static DebugMessage Empty = new DebugMessage("");
+        public StringValue Message { get; }
+
+        public DebugMessage(StringValue message)
+        {
+            Message = message;
+        }
+
+
+        public MessageType Type() => MessageType.Debug;
+        public override string ToString() => $"Debug message {Message}";
+        public bool CanSerialize(string messageType) => false;
+        public byte[] Serialize() => throw new NotImplementedException();
+        public int GetAddress() => 0;
+        public bool CanDeserialize(byte messageType, byte messageSize) => messageType == (byte)Type();
+        public object Deserialize(IBinaryReader reader, byte? messageSize) => new DebugMessage(reader.ReadString(messageSize.GetValueOrDefault()));
+    }
+}
